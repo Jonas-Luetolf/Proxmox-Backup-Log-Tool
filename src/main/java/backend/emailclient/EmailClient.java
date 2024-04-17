9348476f866bca958a0fac04be02f68921073882
jonas <jonas.luetolf@outlook.com>
2024-04-17 14:12:19 +0200

    EmailClient implement login, logout & getEmailsFrom

--- src/main/java/backend/emailclient/EmailClient.java
+++ src/main/java/backend/emailclient/EmailClient.java
@@ -1,38 +1,84 @@
 package backend.emailclient;
-import jdk.jshell.spi.ExecutionControl;
-import java.util.ArrayList;
+import jakarta.mail.*;
+
+import java.util.Properties;
 
 public class EmailClient {
-    private String emailAddr;
-    private String password;
-    private String host;
-    private boolean sslEnable;
+    private final String emailAddr;
+    private final String password;
+    private final String host;
+    private Session session = null;
+    public Store emailStore = null;
+    private final Properties props = new Properties();
 
-    public EmailClient(String emailAddr, String password, String host, boolean sslEnable) {
+    public EmailClient(String emailAddr, String password, String host, int port, boolean sslEnable) {
         this.emailAddr = emailAddr;
         this.password = password;
         this.host = host;
-        this.sslEnable = sslEnable;
+
+        // set Properties used later in session
+        props.setProperty("mail.imap.host", host);
+        props.setProperty("mail.imap.port", String.valueOf(port));
+        props.setProperty("mail.imap.ssl.enable", String.valueOf(sslEnable));
+
     }
 
     /**
      *Logs into the Email server
      */
     public void login(){
+        /* only log in if there isn't a session */
+        if (session == null){
+
+            try {
+                session = Session.getInstance(props);
+                this.emailStore = session.getStore("imaps");
+                this.emailStore.connect(host,emailAddr,password);
+            }
+
+            catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 
     /**
      * Logs out of the Email server
+     *
+     * @implNote MessagingException ignored
      */
     public void logout(){
+        try {
+            if (this.emailStore != null) this.emailStore.close();
+
+            /* set session and email store to null so can be logged in later */
+            this.session = null;
+            this.emailStore = null;
+        }
+
+        /* MessagingException can be ignored
+        * if thrown the emailStore isn't open */
+        catch (MessagingException ignored) {}
     }
 
     /**
      * gets all emails from a specific Email folder
+     * @implNote the email folder won't be closed
+     *
      * @param folderName The folder name to get data from
      * @return an ArrayList with all Emails in the specified folder
      */
-    public ArrayList getEmailsFrom(String folderName){
-        return null;
+    public Message[] getEmailsFrom(String folderName){
+        try {
+            Folder emailFolder = this.emailStore.getFolder(folderName);
+            emailFolder.open(Folder.READ_WRITE);
+
+            return emailFolder.getMessages();
+        }
+
+        catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
